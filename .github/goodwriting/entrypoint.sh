e0be75270079631e1117e4077440d963219cecc8
PiDinoSauR2804 <ngodinhluyennht@gmail.com>
2025-06-20 08:47:52 +0000

    Add GitHub Action to lint markdown files using write-good

--- .github/goodwriting/entrypoint.sh
+++ .github/goodwriting/entrypoint.sh
@@ -6,10 +6,11 @@ echo "📝 Running write-good on all .md files"
 
 found=false
 
-for file in $(find . -type f -name "*.md"); do
+ffind . -type f -name "*.md" -print0 | while IFS= read -r -d '' file; do
   echo "🔍 Checking $file"
   write-good "$file" || found=true
 done
+
 
 if [ "$found" = true ]; then
   echo "❌ Some issues were found by write-good"
